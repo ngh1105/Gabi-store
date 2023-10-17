8b72d146010335d250a151eedadd9a5ecac48e39
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-10-17 12:10:02 +0700

    base folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from 'routes';
+import "styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
