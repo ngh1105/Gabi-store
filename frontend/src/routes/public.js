3e9e7be206e288d558b398806ea67742cf5dfa4f
Minh <107633556+QuangMinh65@users.noreply.github.com>
2023-10-17 16:24:42 +0700

    code 17/10 lan 1

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -1,6 +1,7 @@
 import { PublicLayout } from "layouts/public";
 import { HomePage } from "pages/public/home";
 import { BlogPage } from "pages/public/blog";
+import { BlogDetail } from "pages/public/blog/blogDetail";
 export const publicRoutes = {
     path: '/',
     element: <PublicLayout />,
@@ -8,6 +9,14 @@ export const publicRoutes = {
         {
             path: '/',
             element: <HomePage />,
+        },
+        {
+            path: '/blog',
+            element: <BlogPage />,
+        },
+        {
+            path: '/blogDetail',
+            element: <BlogDetail />,
         },
     ]
 };
