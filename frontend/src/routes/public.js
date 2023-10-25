3a89d28a832ff2ef9a5692d1ff38a5ef315f95fe
Minh <107633556+QuangMinh65@users.noreply.github.com>
2023-10-25 19:17:28 +0700

    code 25/10/2023

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -4,6 +4,17 @@ import { HomePage } from "pages/public/Home";
 import Cart from "pages/public/Cart";
 import Blog from "pages/public/Blog";
 import DeliverInfo from "pages/public/DeliverInfo";
+import Story from "pages/public/Home/Story";
+import Slider from "pages/public/Home/Slider";
+import PostItem from "pages/public/Home/PostItem";
+import Description from "pages/public/Home/Description";
+import Catelog from "pages/public/Home/Catelog"
+import BestSeller from "pages/public/Home/BestSeller";
+import NewProducts from "pages/public/Home/NewProducts";
+import Card from "pages/public/Home/NewProducts/Card";
+import ForgetPassword from "pages/public/ForgetPassword";
+import Product from "pages/public/Product"
+import BlogDetail from "pages/public/Blog/BlogDetail";
  
 export const publicRoutes = {
     path: '/',
@@ -15,6 +26,19 @@ export const publicRoutes = {
         },
         { path: '/cart', element: <Cart/> },
         { path: '/blog', element: <Blog/> },
+        { path: '/blog/:id', element: <BlogDetail/> },
         { path: '/deliverInfo', element: <DeliverInfo/> },
+        { path: '/story', element: <Story/> },
+        { path: '/slider', element: <Slider/> },
+        { path: '/postitem', element: <PostItem/> },
+        { path: '/description', element: <Description/> },
+        { path: '/catelog', element: <Catelog/> },
+        { path: '/bestSeller', element: <BestSeller/> },
+        { path: '/newproducts', element: <NewProducts/> },
+        { path: '/card', element: <Card/> },
+        { path: '/forgetPassword', element: <ForgetPassword/> },
+        { path: '/product', element: <Product/> },
+     
+     
     ]
 };
