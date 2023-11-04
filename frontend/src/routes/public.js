b4edc7900033344a3b523bfffc5fe7fb68261a86
Phúc <97227378+PhucTKW@users.noreply.github.com>
2023-11-04 21:53:34 +0700

    commit sửa Home

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -1,9 +1,23 @@
 import { PublicLayout } from "layouts/public";
-import { HomePage } from "pages/public/Home";
+import { HomePage } from "pages/public/home";
 
 import Cart from "pages/public/Cart";
 import Blog from "pages/public/Blog";
 import DeliverInfo from "pages/public/DeliverInfo";
+<<<<<<< Updated upstream
+=======
+import Story from "pages/public/home/Story";
+import Slider from "pages/public/home/Slider";
+import PostItem from "pages/public/home/PostItem";
+import Description from "pages/public/home/Description";
+import Catelog from "pages/public/home/Catelog"
+import BestSeller from "pages/public/home/BestSeller";
+import NewProducts from "pages/public/home/NewProducts";
+import Card from "pages/public/home/NewProducts/Card";
+import ForgetPassword from "pages/public/ForgetPassword";
+import Product from "pages/public/Product"
+import BlogDetail from "pages/public/Blog/BlogDetail";
+>>>>>>> Stashed changes
  
 export const publicRoutes = {
     path: '/',
