594c92c88e315bd85be0230b3232694f13a6256f
Phúc <97227378+PhucTKW@users.noreply.github.com>
2023-11-04 22:33:53 +0700

    Update

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -1,19 +1,9 @@
 import { PublicLayout } from "layouts/public";
-import { HomePage } from "pages/public/home";
+import { HomePage } from "pages/public/Home";
 
 import Cart from "pages/public/Cart";
 import Blog from "pages/public/Blog";
 import DeliverInfo from "pages/public/DeliverInfo";
-import Story from "pages/public/home/Story";
-import Slider from "pages/public/home/Slider";
-import PostItem from "pages/public/home/PostItem";
-import Description from "pages/public/home/Description";
-import Catelog from "pages/public/home/Catelog"
-import BestSeller from "pages/public/home/BestSeller";
-import NewProducts from "pages/public/home/NewProducts";
-import Card from "pages/public/home/NewProducts/Card";
-import ForgetPassword from "pages/public/ForgetPassword";
-import Product from "pages/public/Product"
 import BlogDetail from "pages/public/Blog/BlogDetail";
  
 export const publicRoutes = {
@@ -27,18 +17,7 @@ export const publicRoutes = {
         { path: '/cart', element: <Cart/> },
         { path: '/blog', element: <Blog/> },
         { path: '/blog/:id', element: <BlogDetail/> },
-        { path: '/deliverInfo', element: <DeliverInfo/> },
-        { path: '/story', element: <Story/> },
-        { path: '/slider', element: <Slider/> },
-        { path: '/postitem', element: <PostItem/> },
-        { path: '/description', element: <Description/> },
-        { path: '/catelog', element: <Catelog/> },
-        { path: '/bestSeller', element: <BestSeller/> },
-        { path: '/newproducts', element: <NewProducts/> },
-        { path: '/card', element: <Card/> },
-        { path: '/forgetPassword', element: <ForgetPassword/> },
-        { path: '/product', element: <Product/> },
-     
+        { path: '/deliverInfo', element: <DeliverInfo/> }
      
     ]
 };
