f2973b70f82bbb2342e11927dd37aeda3c5d9d1e
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-11-05 16:42:51 +0700

    category module

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { categoryProviders } from './category.providers';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [CategoryController],
-    providers: [CategoryService],
+    providers: [CategoryService, ...categoryProviders,],
 })
 export class CategoryModule { }
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { Injectable, Inject, HttpStatus, HttpException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Category } from './entities/category.entity';
@@ -6,88 +6,63 @@ import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
 export class CategoryService {
+    constructor(
+        @Inject('CATEGORY_REPOSITORY')
+        private categoryRepository: typeof Category
+    ) {}
+
     async create(data: CreateCategoryDto) {
-        try {
+        const record = await this.categoryRepository.create({
+            name: data.name,
+        });
 
-            const record = await Category.create({
-                name: data.name,
-            });
-
-            const retData = await record.save();
-            return new CategoryDto(retData);
-        }
-        catch (err) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        const retData = await record.save();
+        return new CategoryDto(retData);
     }
 
     async findAll() {
-        try {
-
-            const data = await Category.findAll<Category>();
-            return data.map(scientist => new CategoryDto(scientist));
-        }
-        catch (err) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        const data = await this.categoryRepository.findAll<Category>();
+        return data.map(scientist => new CategoryDto(scientist));
     }
 
     async findOne(id: number) {
-        try {
+        const record = await this.categoryRepository.findOne<Category>({
+            where: { id: id },
+        });
 
-            const record = await Category.findOne<Category>({
-                where: { id: id },
-            });
-
-            if (!record) {
-                throw new HttpException('No record found', HttpStatus.NOT_FOUND);
-            }
-
-            return new CategoryDto(record);
+        if (!record) {
+            throw new HttpException('No record found', HttpStatus.NOT_FOUND);
         }
-        catch (err) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+        return new CategoryDto(record);
     }
 
     async update(id: number, data: UpdateCategoryDto) {
-        try {
+        const record = await this.categoryRepository.findOne<Category>({
+            where: { id: id },
+        });
 
-            const record = await Category.findOne<Category>({
-                where: { id: id },
-            });
-
-            if (!record) {
-                throw new HttpException('No record found', HttpStatus.NOT_FOUND);
-            }
-
-            record.name = data.name;
-
-            const retData = await record.save();
-            return new CategoryDto(retData);
+        if (!record) {
+            throw new HttpException('No record found', HttpStatus.NOT_FOUND);
         }
-        catch (err) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+        record.name = data.name;
+
+        const retData = await record.save();
+        return new CategoryDto(retData);
     }
 
     async remove(id: number) {
-        try {
+        const record = await this.categoryRepository.findOne<Category>({
+            where: { id: id },
+        });
 
-            const record = await Category.findOne<Category>({
-                where: { id: id },
-            });
-
-            if (!record) {
-                throw new HttpException('No record found', HttpStatus.NOT_FOUND);
-            }
-
-            await record.destroy();
-
-            return "Deleted successfully";
+        if (!record) {
+            throw new HttpException('No record found', HttpStatus.NOT_FOUND);
         }
-        catch (err) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+        await record.destroy();
+
+        return "Deleted successfully";
     }
 }
