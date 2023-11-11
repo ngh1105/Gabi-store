7bd1592a954f6d189603ebc461fabc9fcd57ef83
Minh <107633556+QuangMinh65@users.noreply.github.com>
2023-11-11 13:15:57 +0700

    ..

--- frontend/src/pages/public/Blog/BlogCard/index.jsx
+++ frontend/src/pages/public/Blog/BlogCard/index.jsx
@@ -1,116 +1,121 @@
-import React from 'react';
-import classNames from 'classnames/bind';
-import style from './BlogCard.module.scss';
-import { useNavigate } from 'react-router-dom';
-import Button from 'components/Button';
+import React from "react";
+import classNames from "classnames/bind";
+import style from "./BlogCard.module.scss";
+import { useNavigate } from "react-router-dom";
+import Button from "components/Button";
 
 const cx = classNames.bind(style);
 
 function BlogCard({ item }) {
-    console.log(item);
-    const navigate = useNavigate()
+  console.log(item);
+  const navigate = useNavigate();
 
-    const rootId = item.id;
+  const rootId = item.id;
 
-    const handleDetail = () => {
-        navigate(`/blog/${rootId}`, {
-            state: {
-                item: item,
-            },
-        });
-    };
+  const handleDetail = () => {
+    navigate(`/blog/${rootId}`, {
+      state: {
+        item: item,
+      },
+    });
+  };
 
-    return (
-      <div className="max-w-screen-xl mx-auto">
-      <main className="mt-10">
-        <div className="mb-4 md:mb-0 w-full mx-auto relative">
-          <div className="px-4 lg:px-0">
-            <h2 className="text-4xl font-semibold text-gray-800 leading-tight">
-              Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
-            </h2>
-            <a href="#" className="py-2 text-green-700 inline-flex items-center justify-center mb-2">
-              Cryptocurrency
+  return (
+    <section className="flex items-center bg-white 0 font-poppins dark:bg-gray-900 ">
+      <div className="justify-center flex-1 max-w-screen-xl px-4 py-4 mx-auto text-left ">
+        <div className="mb-10 text-center">
+          <h1 className="text-3xl font-bold capitalize dark:text-white">
+            {" "}
+            Blog List
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 mb-6 md:grid-cols-[40%,1fr] border-gray-200 rounded-md border dark:border-gray-800 gap-2">
+          <div>
+            <img
+              src="https://laforce.vn/wp-content/uploads/2023/06/phong-cach-thoi-trang-nam.jpg"
+              alt
+              className="object-cover w-full rounded-md h-80 md:h-full"
+            />
+          </div>
+          <div className="flex flex-col justify-center px-4 py-4 lg:px-2">
+            <a
+              href="#"
+              className="text-sm font-medium text-gray-600 dark:text-gray-400 "
+            >
+              June 4, 2022
             </a>
-          </div>
-          <img src="https://images.unsplash.com/photo-1587614387466-0a72ca909e16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" className="w-full object-cover lg:rounded" style={{height: '28em'}} />
-        </div>
-        <div className="flex flex-col lg:flex-row lg:space-x-12">
-          <div className="px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4">
-            <p className="pb-6">Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is
-              justice. Six draw
-              you him full not mean evil. Prepare garrets it expense windows shewing do an. She projection advantages
-              resolution son indulgence. Part sure on no long life am at ever. In songs above he as drawn to. Gay was
-              outlived peculiar rendered led six.</p>
-            <p className="pb-6">Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
-              thoroughly
-              partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or
-              attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion
-              expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting
-              uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening
-              newspaper in advantage frankness to concluded unwilling.</p>
-            <p className="pb-6">Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte
-              discovered
-              uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law
-              spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse
-              active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so
-              prospect no as raptures quitting.</p>
-            <div className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
-              Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor
-              expression
-              unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence.
-              Steepest speaking up attended it as. Made neat an on be gave show snug tore.
-            </div>
-            <p className="pb-6">Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me
-              everything.
-              Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage
-              is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry
-              among. Delightful remarkably new assistance saw literature mrs favourable.</p>
-            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">Uneasy barton seeing remark happen his has</h2>
-            <p className="pb-6">Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown
-              oh he common
-              future. Otherwise concealed favourite frankness on be at dashwoods defective at. Sympathize interested
-              simplicity at do projecting increasing terminated. As edward settle limits at in.</p>
-            <p className="pb-6">Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh.
-              Hundred no
-              prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced
-              ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who
-              connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding
-              ask expense are compact. New all paid few hard pure she.</p>
-            <p className="pb-6">Breakfast agreeable incommode departure it an. By ignorant at on wondered relation. Enough at
-              tastes really
-              so cousin am of. Extensive therefore supported by extremity of contented. Is pursuit compact demesne invited
-              elderly be. View him she roof tell her case has sigh. Moreover is possible he admitted sociable concerns. By
-              in cold no less been sent hard hill.</p>
-            <p className="pb-6">Detract yet delight written farther his general. If in so bred at dare rose lose good. Feel and
-              make two real
-              miss use easy. Celebrated delightful an especially increasing instrument am. Indulgence contrasted sufficient
-              to unpleasant in in insensible favourable. Latter remark hunted enough vulgar say man. Sitting hearted on it
-              without me.</p>
-          </div>
-          <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
-            <div className="p-4 border-t border-b md:border md:rounded">
-              <div className="flex py-2">
-                <img src="https://randomuser.me/api/portraits/men/97.jpg" className="h-10 w-10 rounded-full mr-2 object-cover" />
-                <div>
-                  <p className="font-semibold text-gray-700 text-sm"> Mike Sullivan </p>
-                  <p className="font-semibold text-gray-600 text-xs"> Editor </p>
-                </div>
-              </div>
-              <p className="text-gray-700 py-3">
-                Mike writes about technology
-                Yourself required no at thoughts delicate landlord it be. Branched dashwood do is whatever it.
-              </p>
-              <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
-                Follow 
-                <i className="bx bx-user-plus ml-2" />
-              </button>
-            </div>
+            <div className="w-8 pb-1 mb-4 border-b border-gray-600 dark:border-gray-400" />
+            <h2 className="mt-2 mb-4 text-xl font-semibold text-gray-600 dark:text-gray-300 ">
+              Đam Mê Thời Trang, Bạn Phải Biết Những Fashion Này
+            </h2>
+            <p className="mb-4 text-sm text-gray-500 dark:text-gray-400">
+              Làm thế nào để biết mình có đam mê thời trang hay không? Đó là một
+              câu hỏi rất khó trả lời, Chip.vn chỉ muốn nói rằng, đó là khi bạn
+              nhìn thấy một người ăn mặc đẹp và đắm chìm trong bộ trang phục của
+              họ, từ cách họ phối các phụ kiện đi kèm, học hỏi từ style của họ,
+              chứ không nhìn họ bằng con mắt đố kị.
+            </p>
           </div>
         </div>
-      </main>
-    </div>
-
-    );
+        <div className="grid grid-cols-1 mb-6 md:grid-cols-[40%,1fr] border-gray-200 rounded-md border dark:border-gray-800 gap-2">
+          <div>
+            <img
+              src="https://dony.vn/wp-content/uploads/2021/09/ao-quan-mua-xuan.jpg"
+              alt
+              className="object-cover w-full rounded-md h-80 md:h-full"
+            />
+          </div>
+          <div className="flex flex-col justify-center px-4 py-4 lg:px-2">
+            <a
+              href="#"
+              className="text-sm font-medium text-gray-600 dark:text-gray-400 "
+            >
+              June 4, 2022
+            </a>
+            <div className="w-8 pb-1 mb-4 border-b border-gray-600 dark:border-gray-400" />
+            <h2 className="mt-2 mb-4 text-xl font-semibold text-gray-600 dark:text-gray-300 ">
+              Đam Mê Thời Trang, Bạn Phải Biết Những Fashion Này
+            </h2>
+            <p className="mb-4 text-sm text-gray-500 dark:text-gray-400">
+              Làm thế nào để biết mình có đam mê thời trang hay không? Đó là một
+              câu hỏi rất khó trả lời, Chip.vn chỉ muốn nói rằng, đó là khi bạn
+              nhìn thấy một người ăn mặc đẹp và đắm chìm trong bộ trang phục của
+              họ, từ cách họ phối các phụ kiện đi kèm, học hỏi từ style của họ,
+              chứ không nhìn họ bằng con mắt đố kị.
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 mb-6 md:grid-cols-[40%,1fr] border-gray-200 rounded-md border dark:border-gray-800 gap-2">
+          <div>
+            <img
+              src="https://www.novelty.com.vn/public/uploads/images/camo-pants-shorts.jpg"
+              alt
+              className="object-cover w-full rounded-md h-80 md:h-full"
+            />
+          </div>
+          <div className="flex flex-col justify-center px-4 py-4 lg:px-2">
+            <a
+              href="#"
+              className="text-sm font-medium text-gray-600 dark:text-gray-400 "
+            >
+              June 4, 2022
+            </a>
+            <div className="w-8 pb-1 mb-4 border-b border-gray-600 dark:border-gray-400" />
+            <h2 className="mt-2 mb-4 text-xl font-semibold text-gray-600 dark:text-gray-300 ">
+              Đam Mê Thời Trang, Bạn Phải Biết Những Fashion Này
+            </h2>
+            <p className="mb-4 text-sm text-gray-500 dark:text-gray-400">
+              Làm thế nào để biết mình có đam mê thời trang hay không? Đó là một
+              câu hỏi rất khó trả lời, Chip.vn chỉ muốn nói rằng, đó là khi bạn
+              nhìn thấy một người ăn mặc đẹp và đắm chìm trong bộ trang phục của
+              họ, từ cách họ phối các phụ kiện đi kèm, học hỏi từ style của họ,
+              chứ không nhìn họ bằng con mắt đố kị.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default BlogCard;
--- frontend/src/pages/public/Policy/index.jsx
+++ frontend/src/pages/public/Policy/index.jsx
@@ -1,105 +1,123 @@
-import Button from 'components/Button';
-import styles from './Policy.module.scss';
-import classNames from 'classnames/bind';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Button from "components/Button";
+import styles from "./Policy.module.scss";
+import classNames from "classnames/bind";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 function Policy() {
-    return (
-<div className="max-w-screen-lg mx-auto">
-  <main className="mt-10">
-    <div className="mb-4 md:mb-0 w-full mx-auto relative">
-      <div className="px-4 lg:px-0">
-        <h2 className="text-4xl font-semibold text-gray-800 leading-tight">
-          Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
-        </h2>
-        <a href="#" className="py-2 text-green-700 inline-flex items-center justify-center mb-2">
-          Cryptocurrency
-        </a>
-      </div>
-      <img src="https://images.unsplash.com/photo-1587614387466-0a72ca909e16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" className="w-full object-cover lg:rounded" style={{height: '28em'}} />
-    </div>
-    <div className="flex flex-col lg:flex-row lg:space-x-12">
-      <div className="px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4">
-        <p className="pb-6">Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is
-          justice. Six draw
-          you him full not mean evil. Prepare garrets it expense windows shewing do an. She projection advantages
-          resolution son indulgence. Part sure on no long life am at ever. In songs above he as drawn to. Gay was
-          outlived peculiar rendered led six.</p>
-        <p className="pb-6">Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
-          thoroughly
-          partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or
-          attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion
-          expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting
-          uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening
-          newspaper in advantage frankness to concluded unwilling.</p>
-        <p className="pb-6">Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte
-          discovered
-          uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law
-          spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse
-          active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so
-          prospect no as raptures quitting.</p>
-        <div className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
-          Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor
-          expression
-          unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence.
-          Steepest speaking up attended it as. Made neat an on be gave show snug tore.
-        </div>
-        <p className="pb-6">Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me
-          everything.
-          Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage
-          is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry
-          among. Delightful remarkably new assistance saw literature mrs favourable.</p>
-        <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">Uneasy barton seeing remark happen his has</h2>
-        <p className="pb-6">Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown
-          oh he common
-          future. Otherwise concealed favourite frankness on be at dashwoods defective at. Sympathize interested
-          simplicity at do projecting increasing terminated. As edward settle limits at in.</p>
-        <p className="pb-6">Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh.
-          Hundred no
-          prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced
-          ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who
-          connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding
-          ask expense are compact. New all paid few hard pure she.</p>
-        <p className="pb-6">Breakfast agreeable incommode departure it an. By ignorant at on wondered relation. Enough at
-          tastes really
-          so cousin am of. Extensive therefore supported by extremity of contented. Is pursuit compact demesne invited
-          elderly be. View him she roof tell her case has sigh. Moreover is possible he admitted sociable concerns. By
-          in cold no less been sent hard hill.</p>
-        <p className="pb-6">Detract yet delight written farther his general. If in so bred at dare rose lose good. Feel and
-          make two real
-          miss use easy. Celebrated delightful an especially increasing instrument am. Indulgence contrasted sufficient
-          to unpleasant in in insensible favourable. Latter remark hunted enough vulgar say man. Sitting hearted on it
-          without me.</p>
-      </div>
-      <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
-        <div className="p-4 border-t border-b md:border md:rounded">
-          <div className="flex py-2">
-            <img src="https://randomuser.me/api/portraits/men/97.jpg" className="h-10 w-10 rounded-full mr-2 object-cover" />
-            <div>
-              <p className="font-semibold text-gray-700 text-sm"> Mike Sullivan </p>
-              <p className="font-semibold text-gray-600 text-xs"> Editor </p>
-            </div>
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <main className="mt-10">
+        <div className="mb-4 md:mb-0 w-full mx-auto relative">
+          <div className="px-4 lg:px-0">
+            <h2 className="text-4xl font-semibold text-gray-800 leading-tight">
+              Chinh sách
+            </h2>
           </div>
-          <p className="text-gray-700 py-3">
-            Mike writes about technology
-            Yourself required no at thoughts delicate landlord it be. Branched dashwood do is whatever it.
-          </p>
-          <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
-            Follow 
-            <i className="bx bx-user-plus ml-2" />
-          </button>
         </div>
-      </div>
-    </div>
-  </main>
-  {/* main ends here */}
-  {/* footer */}
-</div>
+        <div className="flex flex-col lg:flex-row ">
+          <div className="px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-4/4">
+            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">
+              Chính sách đổi trả
+            </h2>
+            <p className="pb-6">
+              Thật khó chịu nếu phải làm công tác đổi/ trả cho đơn hàng vừa mua!
+            </p>
+            <p className="pb-6">
+              {" "}
+              Và cũng không mấy dễ chịu khi đọc những trang "Chính sách đổi trả"
+              dài ngoằng và đủ thứ điều ràng buộc (thường là như thế)
+            </p>
+            <p className="pb-6">
+              {" "}
+              Do đó GABI Store sẽ viết ngắn gọn nhất có thể, cơ bản là:
+            </p>
+            <p className="pb-6">
+              {" "}
+              - Khách hàng <span>được đổi hoặc trả MIỄN PH</span>Í sản phẩm
+              trong vòng <span>60 ngày</span> kể từ ngày nhận được sản phẩm.
+            </p>
+            <p className="pb-6">
+              - Bất kỳ sản phẩm nào đặt mua tại Coolmate (ngoại trừ Outlet, sản
+              phẩm Săn deal, sản phẩm đặt riêng theo yêu cầu và sản phẩm
+              Pre-Order phiên bản số lượng giới hạn, không về thêm hàng) cũng
+              được áp dụng chính sách này, kể cả sản phẩm đã giặt hay cắt mác.
+              (Ai mà chịu được một chiếc áo quá rộng hay quần sịp quá chật chứ)
+            </p>
+            <p className="pb-6">
+              - Trường hợp trả, Coolmate sẽ hoàn lại tiền hàng (không bao gồm
+              tiền phí vận chuyển nếu có) cho khách trong vòng 24h qua TÀI KHOẢN
+              NGÂN HÀNG của khách (không tính Thứ 7, CN và ngày lễ). Đồng thời,
+              Coolmate sẽ đến tận nơi lấy hàng trả và không thu thêm bất cứ phí
+              gì (Khách hàng cũng có thể tự gởi lại hàng cho Coolmate)
+            </p>
 
-    );
+            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">
+              3 Bước nhanh chóng để đổi trả:
+            </h2>
+            <p className="pb-6">
+              <span>Bước 1</span>: Điền thông tin Đổi/ Trả hàng ở đây, hoặc qua
+              số hotline 0362378890.
+            </p>
+
+            <p className="pb-6">
+              {" "}
+              <span>Bước 2</span>: Nhận cuộc gọi xác nhận từ Coolmate về sản
+              phẩm và thời gian nhận hàng
+            </p>
+            <p className="pb-6">
+              <span>Bước 3</span>: Ngay khi xác nhận chúng tôi sẽ gởi bạn đơn
+              hàng mới (hoặc lấy đơn hàng về), bạn chỉ cần gởi hàng cần đổi/trả
+              cho shipper là được.
+            </p>
+            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">
+              Đối với việc trả hàng:
+            </h2>
+            <p className="pb-6">
+              {" "}
+              Chúng tôi sẽ hoàn lại số tiền hàng (sau khi đã trừ 25.000 VNĐ phí
+              ship hàng) vào tài khoản mà bạn cung cấp tối đa trong 24h làm việc
+              (không tính thứ 7 & Chủ Nhật) sau khi yêu cầu hoàn tiền được CSKH
+              xác nhận.
+            </p>
+            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">
+              Lưu ý:
+            </h2>
+            <p className="pb-6">
+              {" "}
+              - GABI hỗ trợ đổi tối đa 3 lần/1 khách hàng.
+            </p>
+            <p className="pb-6">
+              {" "}
+              - GABI có quyền quyết định dừng việc hỗ trợ đổi trả và trả lại
+              tiền cho khách hàng nếu phát hiện khách hàng sử dụng chính sách để
+              trục lợi (như việc đổi quá nhiều lần).
+            </p>
+            <p className="pb-6">
+              {" "}
+              - Với các đơn sàn TMĐT thì sẽ áp dụng chính sách trả hàng của sàn
+              TMĐT. Tuy nhiên, trường hợp quá thời gian trả hàng của sàn TMĐT sẽ
+              được áp dụng chính sách trả hàng của GABI
+            </p>
+
+            <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">
+              Chúng tôi làm gì với hàng đổi trả:
+            </h2>
+            <p className="pb-6">
+              Áo thun, quần short: thu gom và gởi cho các chương trình từ thiện
+            </p>
+            <p className="pb-6"> Bít tất, boxer: huỷ bỏ 100%</p>
+            <p className="pb-6 text-red-700" >
+              {" "}
+              <Link to="/deliverInfo"> Tiếp tục thanh toán</Link>
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Policy;
