a401c4e87b04e0d56e63b732d9b9e380a8fb5092
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-11-21 12:24:40 +0700

    new footer

--- frontend/src/layouts/public/topbar.jsx
+++ frontend/src/layouts/public/topbar.jsx
@@ -38,7 +38,7 @@ export default function Topbar() {
             <nav className="bg-white border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
-                        <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                        <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Logo" />
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Gabi Store</span>
                     </Link>
                     <div className="flex items-center lg:order-2">
